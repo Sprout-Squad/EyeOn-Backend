e623216cf2b56390900310ac4a31c05e80d3f385
frombunny <rucy011121@gmail.com>
2025-04-05 14:56:36 +0900

    chore: 프로젝트 시작

---
+++ src/main/java/Sprout_Squad/EyeOn/EyeOnApplication.java
@@ -0,0 +1,13 @@
+package Sprout_Squad.EyeOn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyeOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyeOnApplication.class, args);
+	}
+
+}
