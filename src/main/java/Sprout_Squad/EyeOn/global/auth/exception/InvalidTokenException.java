eae74901447543e085225176dd8c2bc03be1c379
frombunny <rucy011121@gmail.com>
2025-04-16 16:23:02 +0900

    refactor: 필요없는 매개변수 삭제

--- src/main/java/Sprout_Squad/EyeOn/global/auth/exception/InvalidTokenException.java
+++ src/main/java/Sprout_Squad/EyeOn/global/auth/exception/InvalidTokenException.java
@@ -1,10 +1,9 @@
 package Sprout_Squad.EyeOn.global.auth.exception;
 
 import Sprout_Squad.EyeOn.global.exception.BaseException;
-import Sprout_Squad.EyeOn.global.response.code.GlobalErrorCode;
 
 public class InvalidTokenException extends BaseException {
-    public InvalidTokenException(GlobalErrorCode globalErrorCode) {
+    public InvalidTokenException() {
         super(AuthErrorCode.INVALID_TOKEN_401);
     }
 }
--- src/main/java/Sprout_Squad/EyeOn/global/auth/oauth2/web/dto/KakaoLoginReq.java
+++ src/main/java/Sprout_Squad/EyeOn/global/auth/oauth2/web/dto/KakaoLoginReq.java
@@ -1,8 +1,8 @@
 package Sprout_Squad.EyeOn.global.auth.oauth2.web.dto;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record KakaoLoginReq(
-        @NotBlank(message = "인가 코드는 비어 있을 수 없습니다.")
+        @NotNull(message = "인가 코드는 비어 있을 수 없습니다.")
         String code
 ) {}
