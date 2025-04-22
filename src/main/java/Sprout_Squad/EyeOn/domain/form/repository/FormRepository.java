8436208e158fb63250c9dfcaf08e961b6128dcba
frombunny <rucy011121@gmail.com>
2025-04-22 16:40:36 +0900

    feat: 문서 양식 조회 로직 작성

--- src/main/java/Sprout_Squad/EyeOn/domain/form/repository/FormRepository.java
+++ src/main/java/Sprout_Squad/EyeOn/domain/form/repository/FormRepository.java
@@ -2,13 +2,16 @@ package Sprout_Squad.EyeOn.domain.form.repository;
 
 import Sprout_Squad.EyeOn.domain.document.entity.enums.DocumentType;
 import Sprout_Squad.EyeOn.domain.form.entity.Form;
+import Sprout_Squad.EyeOn.domain.form.exception.FormNotFoundException;
 import Sprout_Squad.EyeOn.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface FormRepository extends JpaRepository<Form, Long> {
     List<Form> findAllByUserAndFormType(User user, DocumentType formType);
-    Optional<Form> findByFormId(Long formId);
+
+    default Form getFormByFormId(Long id){
+        return findById(id).orElseThrow(FormNotFoundException::new);
+    }
 }
--- src/main/java/Sprout_Squad/EyeOn/domain/form/web/dto/GetFormRes.java
+++ src/main/java/Sprout_Squad/EyeOn/domain/form/web/dto/GetFormRes.java
@@ -2,18 +2,17 @@ package Sprout_Squad.EyeOn.domain.form.web.dto;
 
 import Sprout_Squad.EyeOn.domain.document.entity.enums.DocumentType;
 import Sprout_Squad.EyeOn.domain.form.entity.Form;
-import Sprout_Squad.EyeOn.domain.user.web.dto.GetUserInfoRes;
 
 import java.time.LocalDateTime;
 
-public record GetOneFormRes(
+public record GetFormRes(
         String name,
         LocalDateTime createdAt,
         DocumentType formType,
         String imageUrl
 ) {
-    public static GetOneFormRes of(Form form){
-        return new GetOneFormRes(
+    public static GetFormRes of(Form form){
+        return new GetFormRes(
                 form.getName(),
                 form.getCreatedAt(),
                 form.getFormType(),
