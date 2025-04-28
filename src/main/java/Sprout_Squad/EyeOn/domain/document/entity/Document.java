be0c96b7b55f2470fcb9248486429355d3d2a47e
frombunny <rucy011121@gmail.com>
2025-04-28 23:52:15 +0900

    feat: Document와 Form 사이 연관관계 추가

--- src/main/java/Sprout_Squad/EyeOn/domain/document/entity/Document.java
+++ src/main/java/Sprout_Squad/EyeOn/domain/document/entity/Document.java
@@ -1,6 +1,7 @@
 package Sprout_Squad.EyeOn.domain.document.entity;
 
 import Sprout_Squad.EyeOn.domain.document.entity.enums.DocumentType;
+import Sprout_Squad.EyeOn.domain.form.entity.Form;
 import Sprout_Squad.EyeOn.domain.user.entity.User;
 import Sprout_Squad.EyeOn.global.entity.BaseEntity;
 import jakarta.persistence.*;
@@ -21,6 +22,10 @@ public class Document extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "form_id")
+    private Form form;
 
     @Column(name = "document_name")
     private String documentName;
