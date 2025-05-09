1ddbdb3aedf195328a61bb991ef2c74cc3370b2f
frombunny <rucy011121@gmail.com>
2025-05-10 07:05:23 +0900

    refactor: OpenAiConfig에 헤더 생성 로직 수정

--- src/main/java/Sprout_Squad/EyeOn/global/config/OpenAiConfig.java
+++ src/main/java/Sprout_Squad/EyeOn/global/config/OpenAiConfig.java
@@ -1,7 +1,11 @@
 package Sprout_Squad.EyeOn.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class OpenAiConfig {
@@ -18,4 +22,17 @@ public class OpenAiConfig {
     public String getModel() {
         return model;
     }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public HttpHeaders createOpenAiHeaders(MediaType contentType) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(apiKey);
+        headers.setContentType(contentType); // application/json, multipart/form-data
+        return headers;
+    }
+
 }
