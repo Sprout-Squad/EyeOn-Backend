a14f37913130e8908fc5e2cdc52e7386eb7e9245
frombunny <rucy011121@gmail.com>
2025-05-10 13:18:32 +0900

    feat: OpenAiService 작성

--- src/main/java/Sprout_Squad/EyeOn/global/external/exception/ExternalErrorCode.java
+++ src/main/java/Sprout_Squad/EyeOn/global/external/exception/ExternalErrorCode.java
@@ -10,7 +10,8 @@ import static Sprout_Squad.EyeOn.global.constant.StaticValue.INTERNAL_SERVER_ERR
 @AllArgsConstructor
 public enum ExternalErrorCode implements BaseResponseCode {
     CLOVA_REQUEST_ERROR_500("CLOVA_REQUEST_ERROR_500", INTERNAL_SERVER_ERROR, "CLOVA OCR 호출 중 에러가 발생했습니다."),
-    IMAGE_ENCODE_ERROR_500("IMAGE_ENCODE_ERROR_500", INTERNAL_SERVER_ERROR, "파일 인코딩 중 에러가 발생했습니다.");
+    IMAGE_ENCODE_ERROR_500("IMAGE_ENCODE_ERROR_500", INTERNAL_SERVER_ERROR, "파일 인코딩 중 에러가 발생했습니다."),
+    OPENAI_REQUEST_ERROR_500("OPENAI_REQUEST_ERROR_500", INTERNAL_SERVER_ERROR, "OpenAI 호출 중 에러가 발생했습니다.");
 
     private final String code;
     private final int httpStatus;
