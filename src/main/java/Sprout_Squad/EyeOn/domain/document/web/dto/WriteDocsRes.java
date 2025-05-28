f6cffda8859c42c41081f49af9b5c8ce10e859da
frombunny <rucy011121@gmail.com>
2025-05-28 17:13:25 +0900

    hotfix: 문서 작성 완료시 docuemntId 반환

--- src/main/java/Sprout_Squad/EyeOn/domain/document/web/dto/WriteDocsRes.java
+++ src/main/java/Sprout_Squad/EyeOn/domain/document/web/dto/WriteDocsRes.java
@@ -5,13 +5,14 @@ import Sprout_Squad.EyeOn.domain.document.entity.Document;
 import java.time.LocalDateTime;
 
 public record WriteDocsRes(
+        Long documentId,
         String documentName,
         LocalDateTime createdDate,
         String imageUrl,
         String pdfUrl
 ) {
     public static WriteDocsRes from(Document document) {
-        return new WriteDocsRes(document.getDocumentName(), document.getCreatedAt(), document.getDocumentImageUrl(),
+        return new WriteDocsRes(document.getId(), document.getDocumentName(), document.getCreatedAt(), document.getDocumentImageUrl(),
                 document.getDocumentUrl());
     }
 }
