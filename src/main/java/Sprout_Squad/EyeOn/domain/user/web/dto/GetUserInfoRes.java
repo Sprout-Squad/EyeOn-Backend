13dfb56a6c4d1f9445cbf6612141c98fb3ae0b3f
frombunny <rucy011121@gmail.com>
2025-05-29 12:00:13 +0900

    hotfix: 사용자 정보 조회 시, 반환값 추가

--- src/main/java/Sprout_Squad/EyeOn/domain/user/web/dto/GetUserInfoRes.java
+++ src/main/java/Sprout_Squad/EyeOn/domain/user/web/dto/GetUserInfoRes.java
@@ -1,9 +1,12 @@
 package Sprout_Squad.EyeOn.domain.user.web.dto;
 
 import Sprout_Squad.EyeOn.domain.user.entity.User;
+import Sprout_Squad.EyeOn.domain.user.entity.enums.Gender;
 
 public record GetUserInfoRes(
         String username,
+        Gender gender,
+        String residentNumber,
         String profileImageUrl,
         String address,
         String phoneNumber,
@@ -12,6 +15,8 @@ public record GetUserInfoRes(
     public static GetUserInfoRes from(User user) {
         return new GetUserInfoRes(
                 user.getName(),
+                user.getGender(),
+                user.getResidentNumber(),
                 user.getProfileImageUrl(),
                 user.getAddress(),
                 user.getPhoneNumber(),
